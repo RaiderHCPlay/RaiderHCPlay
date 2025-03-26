0bbdd2f02740d990216af35cf2918ef32d1fbca0
RaiderHCPlay <fabryckipawel@gmail.com>
2025-03-26 13:48:33 +0100

    feat(website): started adding tech and projects

--- src/components/layout/home/Projects.tsx
+++ src/components/layout/home/Projects.tsx
@@ -1,3 +1,34 @@
+'use client'
+
+import { useState, useEffect, useCallback } from 'react'
+
 export default function ProjectsComponent() {
-    return <div className={'projects-content'}></div>
+    const [projects, setProjects] = useState<Projects[]>([])
+
+    const getProjects = useCallback(() => {
+        fetch('/projects.json', {}).then(async (response) => {
+            if (response.ok) {
+                setProjects(await response.json())
+            }
+        })
+    }, [])
+
+    useEffect(() => {
+        getProjects()
+    }, [])
+
+    return (
+        <>
+            {projects && projects.length > 0 && (
+                <div className={'projects-content'}>
+			{projects.map((project, i) => {
+				return ( 
+				<div key={i}>
+					{project.name}
+				</div>)
+			})}
+		</div>
+            )}
+        </>
+    )
 }
--- src/components/layout/home/Tech.tsx
+++ src/components/layout/home/Tech.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useLayoutEffect, useCallback } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import Image from 'next/image'
 
 export default function TechComponent() {
@@ -14,7 +14,7 @@ export default function TechComponent() {
         })
     }, [])
 
-    useLayoutEffect(() => {
+    useEffect(() => {
         fetchTech()
     }, [])
 
