df1df1280cba67fcf975a07dc03cc4ecade7dedc
Paweł Fabrycki <fabryckipawel@gmail.com>
2025-04-03 22:46:07 +0200

    feat(scripts/gen_readme): added `***`

--- scripts/gen_readme.ts
+++ scripts/gen_readme.ts
@@ -50,6 +50,7 @@ ${iconsArray.join(" ")}
 	<a href="https://wakatime.com/@RaiderHCPlay"><img src="https://wakatime.com/share/@RaiderHCPlay/adf53bfd-d925-41d8-b6d5-9189284b7615.png" /></a>
 </details>
 
+***
 ### Media
 
 ${mediaArray.join(" ")}
