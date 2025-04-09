12abbbfb3a2f8961ff5d60677ec91f7f97db07b9
RaiderHCPlay <fabryckipawel@gmail.com>
2025-04-09 12:52:02 +0200

    feat(colors): made them less neon

--- scripts/gen_colors.ts
+++ scripts/gen_colors.ts
@@ -7,6 +7,8 @@ writeFile(
 	--base: ${palette.base};
     	--pink: ${palette.pink};
     	--text: ${palette.text};
+	--subtext0: ${palette.subtext0};
+	--subtext1: ${palette.subtext1};
         --purple: ${palette.purple};
         --crust: ${palette.crust};
         --mantle: ${palette.mantle};
@@ -14,6 +16,9 @@ writeFile(
         --surface0: ${palette.surface0};
         --surface1: ${palette.surface1};
         --surface2: ${palette.surface2};
+	--overlay0: ${palette.overlay0};
+	--overlay1: ${palette.overlay1};
+	--overlay2: ${palette.overlay2};
         --yellow: ${palette.yellow};
         --peach: ${palette.peach};
        	--green: ${palette.green};
