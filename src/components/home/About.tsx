40a29831a0bb87382f286772f5e659585234b4f6
RaiderHCPlay <fabryckipawel@gmail.com>
2025-06-21 21:33:29 +0200

    fix: updated text in about component

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -4,9 +4,9 @@ export default function AboutSection() {
       <h2>Something about me</h2>
       Hey, I’m Paweł (aka RaiderHCPlay) — a student & self-taught dev coding in
       Neovim, building UIs with TypeScript, React & Sass, and experimenting with
-      Tailwind and Ruby. When I’m not coding, I’m watching anime, reading manga
-      (JoJo is peak), or destroying shadows in Persona 3 — all while vibing to
-      peak soundtracks. Currently cooking something cool. Stay tuned.
+      Tailwind and Ruby. When I’m not coding, I’m watching anime, reading manga,
+      or slaying demons in Doom — all while vibing to peak soundtracks.
+      Currently cooking something cool. Stay tuned.
     </div>
   );
 }
