3cd64c9155bb99dafb5315967bc3eba0317094c1
RaiderHCPlay <fabryckipaweldev@gmail.com>
2025-11-19 21:39:11 +0100

    fix(components): remove old components

--- src/lib/components/home/About.svelte
+++
@@ -1,7 +0,0 @@
-<div class="about-content">
-	<h2>Something about me</h2>
-	Hey, I’m Paweł (aka RaiderHCPlay) => a student & web dev coding in Neovim. I love experimenting with
-	new tech like Ruby or Rust. When I’m not coding, you'll find me watching anime, reading manga, or playing
-	a peak game like Persona 3 Reload. Currently, I focus on understanding others' code to learn, and building
-	projects to sharpen my skills. Also, I enjoy Svelte btw.
-</div>
