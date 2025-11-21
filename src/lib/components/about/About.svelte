65786c49162ec15aa8e05b5d88815b38c09adbd6
RaiderHCPlay <fabryckipaweldev@gmail.com>
2025-11-21 11:42:15 +0100

    feat(about): add some content about me

---
+++ src/lib/components/about/About.svelte
@@ -0,0 +1,11 @@
+<div>
+	Hello! I'm <strong>RaiderHCPlay</strong>, a full-stack web developer and student. I love coding in
+	<strong>TypeScript</strong>
+	but enjoy experimenting with <strong>Rust</strong> and <strong>Ruby</strong>. My favorite
+	full-stack framework is <strong>SvelteKit</strong> — it’s clean, easy, and the icon is the coolest
+	(because it's orange). When I’m not coding, I watch anime, read manga, listen to music, or play
+	games like <em>Persona 3 Reload</em> or <em>DOOM</em>. I also enjoy gazing at the full moon.
+	Lately, I’ve been exploring desktop apps with <strong>Tauri</strong>, diving into how stuff like
+	compilers work, and learning to read and understand other people’s code. Because I value
+	simplicity, comfort and flexibility, I code in Neovim on Arch Linux with Hyprland.
+</div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import About from '$lib/components/about/About.svelte';
+	import Github from '$lib/components/about/Github.svelte';
+	import Interests from '$lib/components/about/Interests.svelte';
+	import Tech from '$lib/components/about/Tech.svelte';
+</script>
+
+<div>
+	<div>
+		<a href="/">cd ..</a>
+		<h1>About me</h1>
+	</div>
+	<div>
+		<About />
+		<Tech />
+	</div>
+	<div>
+		<Interests />
+		<Github />
+	</div>
+</div>
