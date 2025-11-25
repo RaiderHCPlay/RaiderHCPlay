f42b6ed5410ed3be90aab668496f6967874454ea
RaiderHCPlay <fabryckipaweldev@gmail.com>
2025-11-25 22:18:02 +0100

    fix(error page): styled properly

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,3 +1,20 @@
-<h2 class="not-found">
+<div class="not-found">
 	Sorry, but this route doesn't exist. <a href="/">cd /home</a>
-</h2>
+</div>
+
+<style>
+	div {
+		display: flex;
+		height: 90%;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+	}
+	a {
+		margin-top: 10px;
+		font-size: clamp(12px, 2vw, 16px);
+		color: var(--accent);
+		text-decoration: none;
+	}
+</style>
